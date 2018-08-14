a7fed18c5569453e1089f7040671e64468a2b9fa
Scott <wsc@iri-labs.com>
2018-08-14 21:54:12 +0200

    docs updated, intended for gddo viewers.

--- doc.go
+++ doc.go
@@ -2,4 +2,13 @@
 // code is governed by a license that can be found in the License file.
 
 // Package sio provides support for audio capture and playback.
+//
+// Package sio is in alpha, under development.  Playback and capture
+// interfaces are more mature than duplex and device interfaces.  Linux/ALSA
+// and darwin have worked on a few test systems.  This package intends to
+// be open-ended with respect to platform support, hopefully one day also
+// including drivers from the likes of http://periph.io as upstream providers.
+//
+// You are welcome to join the discussion and help us meet this goal at
+// http://github.com/zikichombo/sio
 package sio /* import "zikichombo.org/sio" */
