300944a34d55fa0c863eb16b365d07a2c4fe2e33
Scott <wsc@iri-labs.com>
2018-09-27 19:45:13 +0200

    better doc phrasing

--- libsio/doc.go
+++ libsio/doc.go
@@ -1,7 +1,7 @@
 // Copyright 2018 The ZikiChombo Authors. All rights reserved.  Use of this source
 // code is governed by a license that can be found in the License file.
 
-// Package libsio provides support for the different ports.
+// Package libsio provides some support for implementing the different ports.
 //
 // Package libsio is part of http://zikichombo.org
 package libsio /* import "zikichombo.org/sio/libsio" */
